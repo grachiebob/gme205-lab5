dd12cadaa35149cd67573448c712e5a1aaf19284
unknown <mlroque@up.edu.ph>
2026-03-13 20:47:26 +0800

    Introduce additional spatial subclassess

--- src/spatial.py
+++ src/spatial.py
@@ -12,3 +12,30 @@ class SpatialObject:
         """
         raise NotImplementedError
     
+class Parcel (SpatialObject):
+
+    VALID_ZONES = {'Residential', 'Commercial', 'Industrial'}
+    """
+    From Lab 3
+    """
+
+    def __init__(self, p_id, zone, is_active, geometry_data):
+        if zone not in self.VALID_ZONES:
+            raise ValueError(f"Zone must be one of {self.VALID_ZONES}")
+        super().__init__(geometry_data)
+        self.p_id = p_id
+        self.zone = zone
+        self.is_active = is_active
+
+
+class Building(SpatialObject):
+
+    def __init__(self, geometry_data, floors):
+        super().__init__(geometry_data)
+        self.floors = floors
+    
+class Road(SpatialObject):
+    def __init__(self, geometry_data, width):
+        super().__init__(geometry_data)
+        self.width = width
+    
